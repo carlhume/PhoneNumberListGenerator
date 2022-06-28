52c1d965a753cb2831f19796d57b427b8d1bd806
carl <carl.hume@gmail.com>
2022-06-28 10:30:31 -0400

    Basic contact data can now be retrieved from the database

--- src/com/tds/repository/Repository.java
+++ src/com/tds/repository/Repository.java
@@ -6,22 +6,18 @@ import java.sql.*;
 
 public class Repository {
 
-    // >> cnh >> TODO:  Remove coupling to com.tds.directory package ...
     // >> cnh >> TODO:  Remove the database specifics from source code
+/**
+ * This class will fail to compile until the database variables are introduced.
+ */
+    private static final String DATABASE_CONNECTION_URL = "jdbc:mysql://localhost:3306/phone_directory";
 
-    /**
-     * The store method will fail to compile until the database variables are introduced.
-     * @param contact
-     */
-    public void store( Contact contact ) {
+    // >> cnh >> TODO:  Remove coupling to com.tds.directory package ...
+     public void store( Contact contact ) {
         String INSERT_SQL = "INSERT INTO Contacts( phone_number, given_name, family_name ) VALUES( ?, ?, ? )";
 
-        String url = "jdbc:mysql://localhost:3306/phone_directory";
-        String username = "carl";
-        String password = "pA$$w0rd";
-
         try {
-            Connection connection = DriverManager.getConnection( url, username, password );
+            Connection connection = DriverManager.getConnection(DATABASE_CONNECTION_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
             PreparedStatement insertStatement = connection.prepareStatement( INSERT_SQL, Statement.RETURN_GENERATED_KEYS );
             insertStatement.setString( 1, contact.getTelephoneNumber() );
             insertStatement.setString( 2, contact.getGivenName() );
@@ -34,6 +30,23 @@ public class Repository {
 
     public Contact getContactForPhoneNumber( String phoneNumber ) {
         Contact contact = new Contact();
+
+        String SELECT_CONTACT_SQL = "SELECT phone_number, given_name, family_name FROM Contacts where phone_number=?";
+
+        try {
+            Connection connection = DriverManager.getConnection(DATABASE_CONNECTION_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+            PreparedStatement selectStatement = connection.prepareStatement( SELECT_CONTACT_SQL );
+            selectStatement.setString( 1, phoneNumber );
+            ResultSet results = selectStatement.executeQuery();
+            if( results.next() ) {
+                contact.setTelephoneNumber( results.getString( "phone_number" ) );
+                contact.setGivenName( results.getString( "given_name" ) );
+                contact.setFamilyName( results.getString( "family_name" ) );
+            }
+        } catch ( SQLException e ) {
+            e.printStackTrace();
+        }
+
         return contact;
     }
 
